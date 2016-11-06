4bb6825734c0c9dc9004ae365868741dc6a6a1b3
TerraNova <augustus4400@gmail.com>
2016-11-07 00:26:46 +0800

    userlog.txt

--- examples/main.lua
+++ examples/main.lua
@@ -1,21 +1,39 @@
 local skynet = require "skynet"
 local sprotoloader = require "sprotoloader"
-
+print("---------------->a" )
+sprotoloader = require "sprotoloader"
 local max_client = 64
+print("---------------->1" )
 
-skynet.start(function()
-	print("Server start")
+require "skynet.manager"	-- import skynet.register
+
+local function entry()
+	skynet.error("Server start")
+	skynet.error("---------------->3")
+	print("info 1", skynet.self(),skynet.address(skynet.self()) )
+	skynet.register("mainentry---")
+	print("info 2", skynet.self(),skynet.address(skynet.self()) )
+
+
 	skynet.uniqueservice("protoloader")
-	local console = skynet.newservice("console")
-	skynet.newservice("debug_console",8000)
-	skynet.newservice("simpledb")
-	local watchdog = skynet.newservice("watchdog")
-	skynet.call(watchdog, "lua", "start", {
-		port = 8888,
-		maxclient = max_client,
-		nodelay = true,
-	})
+    --local console = skynet.newservice("console")
+    --skynet.newservice("debug_console",8000)
+    --assert(false)
+    --skynet.newservice("simpledb")
+	print("---------------->4" )
+    local watchdog = skynet.newservice ("watchdog")
+	skynet.error("---------------->5" )
+    skynet.call(watchdog, "lua", "start", {	port = 8888, maxclient = max_client,	nodelay = true,} )
+	print("---------------->6" )
 	print("Watchdog listen on ", 8888)
 
-	skynet.exit()
-end)
+
+    print ("---------------->7")
+    skynet.exit ()
+
+end
+
+
+skynet.start(entry)
+
+print("---------------->2" )
