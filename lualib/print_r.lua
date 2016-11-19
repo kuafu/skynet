f52a3dc091da6d9bb44d1799a59f33a7e7a7413d
TerraNova <augustus4400@gmail.com>
2016-11-19 18:23:10 +0800

    m

--- lualib/print_r.lua
+++ lualib/print_r.lua
@@ -11,17 +11,17 @@ local syslog = require "syslog"
 local function print_r(root)
 	local cache = {  [root] = "." }
 	local function _dump(t,space,name)
-		local temp = {}
+		local temp = {""}
 		for k,v in pairs(t) do
 			local key = tostring(k)
 			if cache[v] then
-				tinsert(temp,"\t+" .. key .. " {" .. cache[v].."}")
+				tinsert(temp,"\t+" .. string.format("%6s", key) .. " {" .. cache[v].."}")
 			elseif type(v) == "table" then
 				local new_key = name .. "." .. key
 				cache[v] = new_key
-				tinsert(temp,"\t+" .. key .. _dump(v,space ..(next(t,k) and "|" or " " ).. srep(" ",#key),new_key))
+				tinsert(temp,"\t+" .. string.format("%6s", key) .. _dump(v,space ..(next(t,k) and "|" or " " ).. srep(" ",4),new_key))
 			else
-				tinsert(temp,"\t+" .. key .. " [" .. tostring(v).."]")
+				tinsert(temp,"\t+" .. string.format("%6s", key) .. " [" .. tostring(v).."]")
 			end
 		end
 		return tconcat(temp,"\r\n"..space)
